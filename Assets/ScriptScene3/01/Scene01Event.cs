97d9fbf7e88999724ddf9e1ffb2ed81a955cd2bd
ChaosPrincess1987 <atsadawutnantachan@gmail.com>
2025-11-20 23:27:45 +0700

    10

--- Assets/ScriptScene3/01/Scene01Event.cs
+++ Assets/ScriptScene3/01/Scene01Event.cs
@@ -1,34 +1,90 @@
 using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Scene01Event : MonoBehaviour
+
 {
+
     public GameObject fadeScreenIn;
+
     public GameObject mom;
+
     public GameObject lantom;
+
     public GameObject textbox;
 
     public AudioSource Ah;
+
     public AudioSource Amm;
 
+    public GameObject nextButton;
+
+    public int eventPos = 0;
+
     void Start()
+
     {
+
         StartCoroutine(EventStarter());
+
     }
 
     IEnumerator EventStarter()
+
     {
+
         yield return new WaitForSeconds(2);
+
         fadeScreenIn.SetActive(false);
+
         mom.SetActive(true);
+
         yield return new WaitForSeconds(2);
+
         mom.SetActive(false);
 
+        eventPos = 1;
+
         textbox.SetActive(true);
+
+        nextButton.SetActive(true);
+
         Ah.Play();
+
+    }
+
+    IEnumerator EventOne()
+
+    {
+
+        nextButton.SetActive(false);
+
+        textbox.SetActive(true);
+
+        nextButton.SetActive(true);
+
         yield return new WaitForSeconds(2);
+
         lantom.SetActive(true);
+
+    }
+
+    public void NextButton()
+
+    {
+
+        if (eventPos == 1)
+
+        {
+
+            StartCoroutine(EventOne());
+
+            eventPos = 2;
+
+        }
 
     }
 
